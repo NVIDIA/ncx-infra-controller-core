NVIDIA/bare-metal-manager-core

feat(rest-api): add runtime-managed external JWT issuers
Trusting a new JWT issuer means editing the auth ConfigMap and rolling every
API pod, so onboarding an identity provider is an operator task with downtime
rather than an API call. This adds a DB-backed issuer table and a Provider
Admin API to register, list, get, and delete external issuers at runtime.
There is no update endpoint: an issuer changes by delete plus create, so trust
is never edited in place. An empty issuer table leaves today's ConfigMap-only
behavior untouched, and the whole feature is gated on one condition,
DynamicIssuersEnabled.

The rest is what it takes for that to hold with several replicas serving
tokens:

- Each replica polls the issuer table every 30 s and rebuilds only the rows
  whose configuration signature changed, so a write on one replica converges
  everywhere without LISTEN/NOTIFY.
- Every fetched key set is persisted on its row under a stale-write predicate,
  so a restarting replica can verify tokens before it reaches an identity
  provider, and a replica adopts a peer's newer keys on reload.
- A token naming an issuer this replica has not loaded yet resolves from the
  table alone and is published under the issuer advisory lock, so a token
  minted seconds after a create is accepted without an identity-provider
  round-trip or a reload.
- Registration never contacts the identity provider, so it succeeds while that
  provider is unreachable. The issuer reads back as Pending until a background
  fetch lands and then Ready; Pending issuers are retried every 10 s while
  ready ones stay on the 15 min refresh.
- A key set nothing has confirmed within maxJWKSAge (24 h) fails closed and
  forces a refresh before it is used again, fetches are bounded by a 1 MiB
  response cap and a per-issuer timeout of at most 60 s, JWKS fetches and
  issuer resolutions are deduplicated with singleflight, and the negative
  caches for unknown issuers and key IDs are bounded.
- Name, issuer URL, and 

rest-api/auth/pkg/authentication/keycloak_test.go
@@ -878,7 +878,7 @@ func TestAuthProcessor_KeycloakFlowWithMockJWKS(t *testing.T) {
 
 	// Setup JWT origin config with Keycloak token origin
 	joCfg := config.NewJWTOriginConfig()
-	joCfg.AddConfig("keycloak", jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, true, nil, nil)
+	joCfg.AddConfig(jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, true, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := joCfg.UpdateAllJWKS(); err != nil {
@@ -1072,7 +1072,7 @@ func TestAuthProcessor_KeycloakServiceAccountsDisabled(t *testing.T) {
 
 	// Setup JWT origin config with Keycloak token origin
 	joCfg := config.NewJWTOriginConfig()
-	joCfg.AddConfig("keycloak", jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, keycloakConfigDisabled.ServiceAccountEnabled, nil, nil)
+	joCfg.AddConfig(jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, keycloakConfigDisabled.ServiceAccountEnabled, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := joCfg.UpdateAllJWKS(); err != nil {

rest-api/auth/pkg/authentication/middleware_test.go
@@ -102,8 +102,8 @@ func TestAuthProcessor(t *testing.T) {
 	defer func() { testServer.Close() }()
 
 	joCfg := config2.NewJWTOriginConfig()
-	joCfg.AddConfig("ssa", "ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	joCfg.AddConfig("kas", "authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	joCfg.AddConfig("ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	joCfg.AddConfig("authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := joCfg.UpdateAllJWKS(); err != nil {
@@ -500,8 +500,8 @@ func TestValidateJWTTokens_WithoutExpiration(t *testing.T) {
 	defer func() { testServer.Close() }()
 
 	cfg := config2.NewJWTOriginConfig()
-	cfg.AddConfig("ssa", "ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	cfg.AddConfig("kas", "authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	cfg.AddConfig("ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	cfg.AddConfig("authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data for testing
 	err := cfg.UpdateAllJWKS()
@@ -689,8 +689,8 @@ func TestHandlerInterface_TokenOriginRouting(t *testing.T) {
 
 	// Setup JWT origin config with multiple token origins
 	joCfg := config2.NewJWTOriginConfig()
-	joCfg.AddConfig("ssa", ssaIssuer, testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	joCfg.AddConfig("kas", kasIssuer, testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	joCfg.AddConfig(ssaIssuer, testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	joCfg.AddConfig(kasIssuer, testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data
 	require.NoError(t, joCfg.UpdateAllJWKS())
@@ -887,11 +887,11 @@ func TestProcessorInterface_ErrorScenarios(t *testing.T) {
 			// Add appropriate config based on the test case origin
 			switch tt.origin {
 			case config2.TokenOriginKasSsa:
-				cfg.AddConfig("ssa", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			case config2.TokenOriginKasLegacy:
-				cfg.AddConfig("kas", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			default:
-				cfg.AddConfig("default", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			}
 
 			require.NoError(t, cfg.UpdateAllJWKS())
@@ -954,9 +954,9 @@ func TestProcessorInterface_Mapping(t *testing.T) {
 
 	// Setup JWT origin config
 	cfg := config2.NewJWTOriginConfig()
-	cfg.AddConfig("ssa", ssaIssuer, testServer.URL, config2.TokenOriginKasSsa, false, nil, nil)
-	cfg.AddConfig("kas", kasIssuer, testServer.URL, config2.TokenOriginKasLegacy, false, nil, nil)
-	cfg.AddConfig("keycloak", keycloakIssuer, testServer.URL, config2.TokenOriginKeycloak, true, nil, nil)
+	cfg.AddConfig(ssaIssuer, testServer.URL, config2.TokenOriginKasSsa, false, nil, nil)
+	cfg.AddConfig(kasIssuer, testServer.URL, config2.TokenOriginKasLegacy, false, nil, nil)
+	cfg.AddConfig(keycloakIssuer, testServer.URL, config2.TokenOriginKeycloak, true, nil, nil)
 
 	require.NoError(t, cfg.UpdateAllJWKS())
 

NVIDIA/ncx-infra-controller-core

rest-api/auth/pkg/authentication/keycloak_test.go
@@ -878,7 +878,7 @@ func TestAuthProcessor_KeycloakFlowWithMockJWKS(t *testing.T) {
 
 	// Setup token origin config with Keycloak token origin
 	toCfg := config.NewTokenOriginConfig()
-	toCfg.AddConfig("keycloak", jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, true, nil, nil)
+	toCfg.AddConfig(jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, true, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := toCfg.UpdateAllJWKS(); err != nil {
@@ -1072,7 +1072,7 @@ func TestAuthProcessor_KeycloakServiceAccountsDisabled(t *testing.T) {
 
 	// Setup token origin config with Keycloak token origin
 	toCfg := config.NewTokenOriginConfig()
-	toCfg.AddConfig("keycloak", jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, keycloakConfigDisabled.ServiceAccountEnabled, nil, nil)
+	toCfg.AddConfig(jwksServer.URL+"/realms/nico", jwksServer.URL+"/realms/nico/protocol/openid-connect/certs", config.TokenOriginKeycloak, keycloakConfigDisabled.ServiceAccountEnabled, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := toCfg.UpdateAllJWKS(); err != nil {

rest-api/auth/pkg/authentication/middleware_test.go
@@ -110,8 +110,8 @@ func TestAuthProcessor(t *testing.T) {
 	defer func() { testServer.Close() }()
 
 	toCfg := config2.NewTokenOriginConfig()
-	toCfg.AddConfig("ssa", "ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	toCfg.AddConfig("kas", "authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	toCfg.AddConfig("ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	toCfg.AddConfig("authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data for testing
 	if err := toCfg.UpdateAllJWKS(); err != nil {
@@ -529,8 +529,8 @@ func TestValidateJWTTokens_WithoutExpiration(t *testing.T) {
 	defer func() { testServer.Close() }()
 
 	cfg := config2.NewTokenOriginConfig()
-	cfg.AddConfig("ssa", "ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	cfg.AddConfig("kas", "authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	cfg.AddConfig("ssa.nvidia.com", testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	cfg.AddConfig("authn.nvidia.com", testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data for testing
 	err := cfg.UpdateAllJWKS()
@@ -718,8 +718,8 @@ func TestHandlerInterface_TokenOriginRouting(t *testing.T) {
 
 	// Setup token origin config with multiple token origins
 	toCfg := config2.NewTokenOriginConfig()
-	toCfg.AddConfig("ssa", ssaIssuer, testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
-	toCfg.AddConfig("kas", kasIssuer, testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
+	toCfg.AddConfig(ssaIssuer, testServer.URL+"/ssa", config2.TokenOriginKasSsa, false, nil, nil)
+	toCfg.AddConfig(kasIssuer, testServer.URL+"/kas", config2.TokenOriginKasLegacy, false, nil, nil)
 
 	// Initialize JWKS data
 	require.NoError(t, toCfg.UpdateAllJWKS())
@@ -916,11 +916,11 @@ func TestProcessorInterface_ErrorScenarios(t *testing.T) {
 			// Add appropriate config based on the test case origin
 			switch tt.origin {
 			case config2.TokenOriginKasSsa:
-				cfg.AddConfig("ssa", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			case config2.TokenOriginKasLegacy:
-				cfg.AddConfig("kas", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			default:
-				cfg.AddConfig("default", tt.issuer, server.URL, tt.origin, false, nil, nil)
+				cfg.AddConfig(tt.issuer, server.URL, tt.origin, false, nil, nil)
 			}
 
 			require.NoError(t, cfg.UpdateAllJWKS())
@@ -983,9 +983,9 @@ func TestProcessorInterface_Mapping(t *testing.T) {
 
 	// Setup token origin config
 	cfg := config2.NewTokenOriginConfig()
-	cfg.AddConfig("ssa", ssaIssuer, testServer.URL, config2.TokenOriginKasSsa, false, nil, nil)
-	cfg.AddConfig("kas", kasIssuer, testServer.URL, config2.TokenOriginKasLegacy, false, nil, nil)
-	cfg.AddConfig("keycloak", keycloakIssuer, testServer.URL, config2.TokenOriginKeycloak, true, nil, nil)
+	cfg.AddConfig(ssaIssuer, testServer.URL, config2.TokenOriginKasSsa, false, nil, nil)
+	cfg.AddConfig(kasIssuer, testServer.URL, config2.TokenOriginKasLegacy, false, nil, nil)
+	cfg.AddConfig(keycloakIssuer, testServer.URL, config2.TokenOriginKeycloak, true, nil, nil)
 
 	require.NoError(t, cfg.UpdateAllJWKS())
 